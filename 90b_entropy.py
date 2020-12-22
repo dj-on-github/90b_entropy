707922bb3cf21515c32377c45fecd266e2419fbb
David Johnston <dj@deadhat.com>
2020-12-21 16:52:43 -0800

    csv output option

--- 90b_entropy.py
+++ 90b_entropy.py
@@ -8,6 +8,8 @@ from mpmath import *
 
 PRECISION = 2048
 
+out_format = "text"
+#out_format = "csv"
 
 #h_in = (input_entropy_rate * n_in) - 262
 
@@ -23,26 +25,33 @@ def output_entropy(n_in,n_out,nw, h_in):
 
 if __name__=="__main__":
 
+    if out_format=="csv":
+        print("algorithm, output_bits, input_bits, efficiency, min_entropy_input_rate")
     for type in ["cbc-mac","sha-256", "sha-384", "sha-512"]:
         if type=="cbc-mac":
             nw = mpf(128)
             n_out = mpf(128)
-            print("AES-CBC-MAC Extraction, n_out = %d" % n_out)
+            if out_format == "text":
+                print("AES-CBC-MAC Extraction, n_out = %d" % n_out)
         elif type=="sha-256":
             nw = mpf(256)
-            print("HMAC/SHA-256 Extraction, n_out = %d" % n_out)
+            if out_format == "text":
+                print("HMAC/SHA-256 Extraction, n_out = %d" % n_out)
             n_out = mpf(256)
         elif type=="sha-384":
-            print("HMAC/SHA-384 Extraction, n_out = %d " % n_out)
+            if out_format == "text":
+                print("HMAC/SHA-384 Extraction, n_out = %d " % n_out)
             nw = mpf(512)
             n_out = mpf(384)
         elif type=="sha-512":
-            print("HMAC/SHA-512 Extraction, n_out = %d" % n_out)
+            if out_format == "text":
+                print("HMAC/SHA-512 Extraction, n_out = %d" % n_out)
             nw = mpf(512)
             n_out = mpf(512)
         else:
             print("Need a working type")
             exit(1)
+
 
         for L in range(2,49):
             n_in = mpf(128*L)
@@ -55,7 +64,10 @@ if __name__=="__main__":
                 ys.append(entropy)
                 if (found == False) and (entropy >= n_out):
                     found = True
-                    print("BITS=",str(n_in).rjust(8),"Bytes=",str(int(n_in/8)).rjust(5), (" efficiency= %3.4f" %(n_out/h_in)).rjust(8),"  Required Min_input_entropy_rate= %3.4f" % x)
+                    if out_format == "text":
+                        print("BITS=",str(n_in).rjust(8),"Bytes=",str(int(n_in/8)).rjust(5), (" efficiency= %3.4f" %(n_out/h_in)).rjust(8),"  Required Min_input_entropy_rate= %3.4f" % x)
+                    elif out_format == "csv":
+                        print(type," , ",int(n_out)," , ",int(n_in)," , ","%3.4f"%(n_out/h_in)," , ","%3.4f"%x) 
 
         #plt.plot(xs,ys,label="L="+str(L))
 
